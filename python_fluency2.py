1f218d7c7499fe43627780c102462a14ffc44ed7
Mark Griffith <markgriffith009@Gmail.com>
2020-01-22 15:06:12 +0000

    fixed issues.

--- python_fluency2.py
+++ python_fluency2.py
@@ -6,7 +6,7 @@ def halved(list):
 print(halved([2,4,6,8,10]))
 
 def only_positive(list):
-    return [num for num in list if num >= 0]
+    return [num for num in list if num > 0]
     
 print(only_positive([-2,4,6,-8,10]))
 
@@ -14,17 +14,17 @@ def sum(list):
     return sum(list)
     
 def stripped_strings(list):
-    return [list for string in list ]
+    return [ re.sub('\W', '', string) for string in list ]
 
 print(stripped_strings(["123456jghfj----"]))
     
 def find_special(list):
-    return [ i for i in range(len(list) if list[i] == "special"]
+    return [ i for i in range(len(list)) if list[i] == "special"]
     
 print(find_special(['spec','special']))
     
 def valid_contacts(list):
-    return contact
+    return [contact for contact in list if len(contact['phone_number']) == 10]
 
 def contact_names(list):
-    return [ list for contact in list if con]
+    return [ contact['name'] for contact in list]
